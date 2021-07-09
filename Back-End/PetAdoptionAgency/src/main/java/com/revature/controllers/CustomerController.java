1ee80c89d9140a344a8fad1e4f770d0e1a40afd6
emmettriddle <emmett.riddle@revature.net>
2021-07-09 17:34:13 -0500

    Customer/Services/impl/controller working

--- Back-End/PetAdoptionAgency/src/main/java/com/revature/controllers/CustomerController.java
+++ Back-End/PetAdoptionAgency/src/main/java/com/revature/controllers/CustomerController.java
@@ -1,5 +1,78 @@
 package com.revature.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.beans.*;
+import com.revature.services.*;
+
+@RestController
+@RequestMapping("/customers")
+@CrossOrigin(origins = "http://localhost:8080")
 public class CustomerController {
+	
+	private CustomerServicesImpl cs;
+	//public Gson gson = new Gson();
+	@Autowired
+	public CustomerController(CustomerServicesImpl cServ) {
+		this.cs = cServ;
+	}
+	
+	//@GetMapping()
+	//public ResponseEntity<List<Pet>> getAllPets(){
+		
+	//	List<Pet> pets = ps.getAll();
+	//	return ResponseEntity.ok(pets);
+	//}
+	
+	@GetMapping(produces="application/json")
+	public List<Customer> getAllCustomer(){
+		
+		List<Customer> customer = cs.getAll();
+		return customer;
+	} 
+	
+	@GetMapping("/{id}")
+	public Customer getById(@PathVariable("id") int id) {
+		Customer customer = cs.getCustomerById(id);
+		return customer;
+	}
+	
+	@PostMapping(path="/addCustomer", consumes = "application/json", produces="application/json")
+	@ResponseStatus(value=HttpStatus.OK)
+	public Customer addCustomer(@RequestBody Customer customer) {
+		
+		//Pet p = gson.fromJson(petJson, Pet.class);		
+		return cs.addCustomer(customer);
+	}
+	
+	@PutMapping(path="/{id}")
+	@ResponseStatus(value=HttpStatus.OK)
+	public Customer updateCustomer(@PathVariable("id") int id, @RequestBody Customer customer) {
+		if (id == customer.getId()) {
+			return cs.updateCustomer(customer);
+		}
+		return null;
+		
+	}
+	
+	@DeleteMapping(path="/{id}")
+	@ResponseStatus(value=HttpStatus.OK)
+	public boolean deleteCustomer(@PathVariable("id") int id) {
+		Customer c = cs.getCustomerById(id);
+		return cs.deleteCustomer(c);
+	}
 
 }
