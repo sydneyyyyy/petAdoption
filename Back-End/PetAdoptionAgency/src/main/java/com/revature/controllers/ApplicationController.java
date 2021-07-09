bc0847f1d0bfe1e12e8e9c100f325a361134f525
emmettriddle <emmett.riddle@revature.net>
2021-07-09 17:12:50 -0500

    Application/services/impl/controller working

--- Back-End/PetAdoptionAgency/src/main/java/com/revature/controllers/ApplicationController.java
+++ Back-End/PetAdoptionAgency/src/main/java/com/revature/controllers/ApplicationController.java
@@ -1,5 +1,79 @@
 package com.revature.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.revature.beans.Application;
+import com.revature.beans.Pet;
+import com.revature.services.ApplicationServicesImpl;
+
+@RestController
+@RequestMapping("/applications")
+@CrossOrigin(origins = "http://localhost:8080")
 public class ApplicationController {
+	
+	private ApplicationServicesImpl as;
+	//public Gson gson = new Gson();
+	@Autowired
+	public ApplicationController(ApplicationServicesImpl appServ) {
+		this.as = appServ;
+	}
+	
+	//@GetMapping()
+	//public ResponseEntity<List<Pet>> getAllPets(){
+		
+	//	List<Pet> pets = ps.getAll();
+	//	return ResponseEntity.ok(pets);
+	//}
+	
+	@GetMapping(produces="application/json")
+	public List<Application> getAllApplication(){
+		
+		List<Application> a = as.getAll();
+		return a;
+	} 
+	
+	@GetMapping("/{id}")
+	public Application getById(@PathVariable("id") int id) {
+		Application a = as.getApplicationById(id);
+		return a;
+	}
+	
+	@PostMapping(path="/addApplication", consumes = "application/json", produces="application/json")
+	@ResponseStatus(value=HttpStatus.OK)
+	public Application addApplication(@RequestBody Application app) {
+		
+		//Pet p = gson.fromJson(petJson, Pet.class);		
+		return as.addApplication(app);
+	}
+	
+	@PutMapping(path="/{id}")
+	@ResponseStatus(value=HttpStatus.OK)
+	public Application updateApplication(@PathVariable("id") int id, @RequestBody Application app) {
+		if (id == app.getId()) {
+			return as.updateApplication(app);
+		}
+		return null;
+		
+	}
+	
+	@DeleteMapping(path="/{id}")
+	@ResponseStatus(value=HttpStatus.OK)
+	public boolean deleteApplication(@PathVariable("id") int id) {
+		Application app = as.getApplicationById(id);
+		return as.deleteApplication(app);
+	}
 
 }
--- Back-End/PetAdoptionAgency/src/main/java/com/revature/services/ApplicationServicesImpl.java
+++ Back-End/PetAdoptionAgency/src/main/java/com/revature/services/ApplicationServicesImpl.java
@@ -1,5 +1,54 @@
 package com.revature.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.revature.beans.Application;
+import com.revature.beans.Pet;
+import com.revature.repos.ApplicationHibernate;
+import com.revature.repos.PetRepo;
+
+@Service
 public class ApplicationServicesImpl implements ApplicationServices {
+	
+	private ApplicationHibernate ah;
+	
+	@Autowired
+	public ApplicationServicesImpl(ApplicationHibernate ah) {
+		this.ah = ah;
+	}
+
+	@Override
+	public List<Application> getAll() {
+		// TODO Auto-generated method stub
+		return (List<Application>) ah.findAll();
+	}
+
+	@Override
+	public Application getApplicationById(int id) {
+		// TODO Auto-generated method stub
+		return ah.findById(id).orElse(null);
+	}
+
+	@Override
+	public Application updateApplication(Application a) {
+		// TODO Auto-generated method stub
+		return ah.save(a);
+	}
+
+	@Override
+	public boolean deleteApplication(Application a) {
+		// TODO Auto-generated method stub
+		ah.deleteById(a.getId());
+		return false;
+	}
+
+	@Override
+	public Application addApplication(Application a) {
+		// TODO Auto-generated method stub
+		return ah.save(a);
+	}
 
 }
