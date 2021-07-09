53626edd8ccf45f2082c149a4dae4edd06169396
Mark Mersman <markmersman@gmail.com>
2021-07-09 00:32:04 -0500

    initial commit, should be able to run and utilize the endpoint for Pet

--- Back-End/PetAdoptionAgency/src/main/java/com/revature/repos/PetHibernate.java
+++ Back-End/PetAdoptionAgency/src/main/java/com/revature/repos/PetHibernate.java
@@ -1,5 +1,58 @@
 package com.revature.repos;
 
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.revature.beans.Pet;
+import com.revature.utils.HibernateUtil;
+
+
+/*
+ * This worked when I tested before we learned Spring Boot.
+ * Not sure we need it now that the PetRepo is available...
+ */
 public class PetHibernate {
 
+	public List<Pet> getAll(){
+		Session s = HibernateUtil.getSession();
+		
+		List<Pet> petList = null;
+		
+		try {
+			petList = s.createQuery("FROM Pet").list();
+			
+		}
+		catch(HibernateException e){
+			//e.printStackTrace();
+			System.out.println("getAll pets issue");
+		}
+		finally {
+			s.close();
+		}
+		
+		return petList;
+	}
+	
+	public void add(Pet pet) {
+		Session s = HibernateUtil.getSession();
+		Transaction t = null;
+		
+		try {
+			t = s.beginTransaction();
+			s.save(pet);
+			t.commit();
+		}
+		catch(HibernateException e) {
+			e.printStackTrace();
+			t.rollback();
+		}
+		finally {
+			s.close();
+		}
+		
+		
+	}
 }
