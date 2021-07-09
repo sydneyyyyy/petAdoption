c1f00fb597be1312ae2d6dd4043b004aad199ac9
emmettriddle <emmett.riddle@revature.net>
2021-07-09 13:22:33 -0500

    Repos have been structured for boot & hibernate

--- Back-End/PetAdoptionAgency/src/main/java/com/revature/repos/CustomerHibernate.java
+++ Back-End/PetAdoptionAgency/src/main/java/com/revature/repos/CustomerHibernate.java
@@ -1,5 +1,11 @@
 package com.revature.repos;
 
-public class CustomerHibernate {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.revature.beans.*;
+
+@Repository
+public interface CustomerHibernate extends JpaRepository<Customer, Integer> {
 
 }
--- Back-End/PetAdoptionAgency/src/main/java/com/revature/repos/PetHibernate.java
+++ Back-End/PetAdoptionAgency/src/main/java/com/revature/repos/PetHibernate.java
@@ -1,5 +1,11 @@
 package com.revature.repos;
 
-public class PetHibernate {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.revature.beans.Pet;
+
+@Repository
+public interface PetHibernate extends JpaRepository<Pet, Integer> {
 
 }
