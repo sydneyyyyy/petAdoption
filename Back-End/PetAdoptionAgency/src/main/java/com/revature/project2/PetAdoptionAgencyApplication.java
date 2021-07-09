30a87438767ae275bb237514f65f9a3c90ee3a54
sydneyyyyy <portersyd5@gmail.com>
2021-07-08 21:30:56 -0400

    created spring-boot with backend and frontend folders

---
+++ Back-End/PetAdoptionAgency/src/main/java/com/revature/project2/PetAdoptionAgencyApplication.java
@@ -0,0 +1,13 @@
+package com.revature.project2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetAdoptionAgencyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetAdoptionAgencyApplication.class, args);
+	}
+
+}
