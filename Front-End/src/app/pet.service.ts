df304ab72faaa67305b977397cd22c889d797332
Gregory Diaz <gregdiaz8624@yahoo.com>
2021-07-12 14:04:57 -0400

    pet services get pet

---
+++ Front-End/src/app/pet.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Pet } from './pet';
+
+@Injectable({ 
+  providedIn: 'root'
+})
+export class PetService {
+  private apiServerUrl = '';
+
+  constructor(private http: HttpClient) { }
+
+  public getPets(): Observable<Pet[]>{
+    return this.http.get<Pet[]>(`${this.apiServerUrl}/pet/all`)
+  }
+
+  public addPet(pet: Pet): Observable<Pet>{
+    return this.http.post<Pet>(`${this.apiServerUrl}/pet/add`, pet)
+  }
+
+  public updatePet(pet: Pet): Observable<Pet>{
+    return this.http.post<Pet>(`${this.apiServerUrl}/pet/add`, pet)
+  }
+}
--- Front-End/src/app/pet.ts
+++ Front-End/src/app/pet.ts
@@ -1,3 +1,11 @@
 export interface Pet {
-    
+    id: number;
+    name: string;
+    breed: string;
+    size: string;
+    age: number;
+    gender: string;
+    price: number;
+    available: boolean;
+    image: string;
 }
