880cede9d2dd592ce9e7e4b4de932c632279c85c
emmettriddle <emmett.riddle@revature.net>
2021-07-15 15:45:39 -0500

    added a getAll application endpoint

--- Back-End/PetAdoptionAgency/src/main/java/com/revature/controllers/ApplicationController.java
+++ Back-End/PetAdoptionAgency/src/main/java/com/revature/controllers/ApplicationController.java
@@ -48,12 +48,11 @@ public class ApplicationController {
 		this.cs = cs;
 	}
 	
-	//@GetMapping()
-	//public ResponseEntity<List<Pet>> getAllPets(){
-		
-	//	List<Pet> pets = ps.getAll();
-	//	return ResponseEntity.ok(pets);
-	//}
+	@GetMapping(produces="application/json")
+	public List<Application> getAllApplication(){
+		List<Application> apps = as.getAll();
+		return apps;
+	}
 	
 	@GetMapping(value="/{eId}" , produces="application/json")
 	public List<Application> getEmpApplications(@PathVariable("eId") int eId){
