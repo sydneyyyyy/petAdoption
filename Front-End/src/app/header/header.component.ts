2d935e7012941b2af9eced276c93072ce6d08834
Mark Mersman <markmersman@gmail.com>
2021-07-18 20:13:38 -0500

    NavBar anchors now rediriect to the home page

--- Front-End/src/app/header/header.component.ts
+++ Front-End/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -9,7 +10,7 @@ export class HeaderComponent implements OnInit {
 
   public isLoggedIn = localStorage.getItem('currentUser');
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
     
@@ -18,5 +19,6 @@ export class HeaderComponent implements OnInit {
   public logout() {
     console.log("logout clicked..");
     localStorage.removeItem('currentUser');
+    this.router.navigate(['home']);
   }
 }
