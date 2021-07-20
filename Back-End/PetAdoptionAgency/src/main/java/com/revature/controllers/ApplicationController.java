a1ba8f6a98f1b8ba6cff02954bcb54768da98639
sydneyyyyy <portersyd5@gmail.com>
2021-07-20 14:26:39 -0400

    changed applicationController back

--- Back-End/PetAdoptionAgency/src/main/java/com/revature/controllers/ApplicationController.java
+++ Back-End/PetAdoptionAgency/src/main/java/com/revature/controllers/ApplicationController.java
@@ -1,5 +1,6 @@
 package com.revature.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.*;
@@ -63,10 +64,12 @@ public class ApplicationController {
 		List<Application> a = as.getAll();
 		List<Application> empAppList = as.getBySpecies(emp.getSpecies());
 		
+
+		
 		//Need to filter out second approval for the employee's own species
 		for (Application app : a){
-			if ((app.getStatus().equals("submitted"))) {empAppList.add(app);}
-			else if ((app.getStatus().equals("secondApproval"))&&(app.getPet().getBreed().getSpecies() != emp.getSpecies())) {
+			// if ((app.getStatus().equals("submitted"))) {empAppList.add(app);}
+			if ((app.getStatus().equals("secondApproval"))&&(app.getPet().getBreed().getSpecies() != emp.getSpecies())) {
 				empAppList.add(app);
 			}
 		}
