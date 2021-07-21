a06a209f972c8b15b8fb6070b86a002a05502766
Mark Mersman <markmersman@gmail.com>
2021-07-20 23:15:34 -0500

    adding cors origin

--- Back-End/PetAdoptionAgency/src/main/java/com/revature/project2/PetAdoptionAgencyApplication.java
+++ Back-End/PetAdoptionAgency/src/main/java/com/revature/project2/PetAdoptionAgencyApplication.java
@@ -36,7 +36,7 @@ public class PetAdoptionAgencyApplication extends SpringBootServletInitializer{
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200","http://p2petadoption.s3-website-us-east-1.amazonaws.com"));
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type", "Accept",
 				"Authorization", "Origin, Accept", "X-Requested-With", "Access-Control-Request-Method", 
 				"Access-Control-Request-Headers"));
